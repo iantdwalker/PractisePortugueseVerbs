9ffa93364ba9b3e2e3972393665ad7ace1f1e5b4
Ian Walker <iantdwalker@gmail.com>
2023-07-16 12:38:01 +0100

    WIP on example question

---
+++ src/components/ExampleQuestion.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-text-field
+   variant="underlined"
+   :prefix="exampleQuestionPrefix"
+   :suffix="exampleQuestionSuffix"
+  >
+
+  </v-text-field>
+</template>
+
+<script setup lang="ts">
+const exampleQuestionPrefix = "Eu estou";
+const exampleQuestionSuffix = "meu jantar";
+</script>
+
+<style scoped>
+
+</style>
