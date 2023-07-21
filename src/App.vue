dbd12f20332c8e7e97a194a7b4e51f42e73db921
Ian Walker <iantdwalker@gmail.com>
2023-07-22 00:19:28 +0100

    Updated app title

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Practise Portuguese Verb Conjugations" />
+      <HelloWorld msg="Practise Portuguese Verbs" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
