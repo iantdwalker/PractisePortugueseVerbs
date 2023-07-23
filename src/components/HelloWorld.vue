27bcd979e0203c4f10240a11b4591a427c3d3512
Ian Walker <iantdwalker@gmail.com>
2023-07-23 01:11:12 +0100

    Add a small blurb for the app's description

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,10 +8,8 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-      What's next?
+      Practise forming sentences in Portuguese using verbs with many different
+      pronouns, tenses, forms and conjugations.
     </h3>
   </div>
 </template>
