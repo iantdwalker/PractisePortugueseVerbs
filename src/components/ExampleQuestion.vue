a212067d957ffdb2bc2fc221ab15e3b53c4a7468
Ian Walker <iantdwalker@gmail.com>
2023-07-23 22:36:40 +0100

    Removed the clearable button as it takes up valuable real estate and can cause confusion with geting a question right (as it shows a green cross!)

--- src/components/ExampleQuestion.vue
+++ src/components/ExampleQuestion.vue
@@ -2,30 +2,14 @@
   <div class="hand-written-font">
     <v-responsive class="mx-auto">
       <v-text-field
+        v-model="userInputValue"
+        @update:modelValue="onModelUpdated"
         :class="displayStyle"
         variant="underlined"
         color="hsla(160, 100%, 37%, 1)"
         :hint="hintText"
-        clearable
-        persistent-clear
-        clear-icon="fas fa-times"
+        persistent-hint
         autofocus
-        active
-        :prefix="exampleQuestionPrefix"
-        :suffix="exampleQuestionSuffix"
-        :rules="[rules.required]"
-        validate-on="blur"
-        :append-icon="questionIcon"
-      >
-      </v-text-field>
-      <v-text-field
-        :class="displayStyle"
-        variant="underlined"
-        color="hsla(160, 100%, 37%, 1)"
-        :hint="hintText"
-        clearable
-        persistent-clear
-        clear-icon="fas fa-times"
         active
         :prefix="exampleQuestionPrefix"
         :suffix="exampleQuestionSuffix"
@@ -40,31 +24,45 @@
 
 <script setup lang="ts">
 import { useDisplay } from "vuetify";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 
 const { name } = useDisplay();
-const hintText = "Gerundio...";
 const exampleQuestionPrefix = "Eu estou";
 const exampleQuestionSuffix = "meu jantar";
 const correctAnswer = "comendo";
 const questionIcon = "fas fa-utensils";
 
 const displayStyle = computed(() => {
+  console.log("display style:" + name.value);
   switch (name.value) {
     case "xs":
-      return "center-text-input input-font-xs additional-text";
+      return "center-text-input question-font-size question-icon";
     case "sm":
-      return "center-text-input input-font-sm additional-text";
+      return "center-text-input question-font-size-sm question-icon-sm";
     case "md":
-      return "center-text-input input-font-md additional-text";
+      return "center-text-input question-font-size-md question-icon-md";
+    case "lg":
+    case "xl":
+    case "xxl":
+      return "center-text-input question-font-size-lgPlus question-icon-lgPlus";
   }
-  return "center-text-input input-font additional-text";
+  return undefined;
 });
 
 const rules = {
-  required: (value: string) =>
-    correctAnswer.startsWith(value) || value === correctAnswer || "Incorrecto!",
+  required: (value: string) => value === correctAnswer || "Incorrect!",
 };
+
+var userInputValue = ref("");
+var hintText = "Gerundio...";
+
+function onModelUpdated(value: string): void {
+  if (value === correctAnswer) {
+    hintText = "Correct!";
+  } else {
+    hintText = "Gerundio...";
+  }
+}
 </script>
 
 <style scoped lang="scss">
@@ -79,29 +77,52 @@ const rules = {
   text-align: center;
 }
 
-.input-font-xs :deep(input) {
+/* extra small - small to large phone */
+.question-font-size :deep(input),
+.question-font-size :deep(.v-text-field__prefix),
+.question-font-size :deep(.v-text-field__suffix) {
   font-size: 1.2em;
 }
 
-.input-font-sm :deep(input) {
-  font-size: 1.5em;
+.question-icon :deep(.v-icon) {
+  font-size: 1.2em;
+  padding-top: 15px;
 }
 
-.input-font-md :deep(input) {
+/* small - small to medium tablet */
+.question-font-size-sm :deep(input),
+.question-font-size-sm :deep(.v-text-field__prefix),
+.question-font-size-sm :deep(.v-text-field__suffix) {
   font-size: 2em;
 }
 
-.input-font :deep(input) {
+.question-icon-sm :deep(.v-icon) {
+  font-size: 2em;
+  padding-top: 25px;
+}
+
+/* medium - large tablet to laptop */
+.question-font-size-md :deep(input),
+.question-font-size-md :deep(.v-text-field__prefix),
+.question-font-size-md :deep(.v-text-field__suffix) {
   font-size: 2.5em;
 }
 
-.additional-text :deep(.v-text-field__prefix),
-:deep(.v-text-field__suffix) {
-  font-size: 1.2em;
+.question-icon-md :deep(.v-icon) {
+  font-size: 2.5em;
+  padding-top: 30px;
 }
 
-.additional-text :deep(.v-icon) {
-  padding-top: 15px;
+/* largePlus - Laptop to desktop, 1080p to 1440p desktop, 4k and ultra-wide */
+.question-font-size-lgPlus :deep(input),
+.question-font-size-lgPlus :deep(.v-text-field__prefix),
+.question-font-size-lgPlus :deep(.v-text-field__suffix) {
+  font-size: 3em;
+}
+
+.question-icon-lgPlus :deep(.v-icon) {
+  font-size: 3em;
+  padding-top: 35px;
 }
 
 // not currently doing anything:
