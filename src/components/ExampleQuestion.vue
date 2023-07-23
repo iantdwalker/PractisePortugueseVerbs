6a251c55bd2f2ca865fd0464eb53d58f6ea11713
Ian Walker <iantdwalker@gmail.com>
2023-07-23 17:45:23 +0100

    Updated display style for example question component

--- src/components/ExampleQuestion.vue
+++ src/components/ExampleQuestion.vue
@@ -1,32 +1,37 @@
 <template>
-  <div class="hand-written">
-    <v-responsive class="mx-auto" max-width="300">
+  <div class="hand-written-font">
+    <v-responsive class="mx-auto">
       <v-text-field
-        class="centered-input"
+        :class="displayStyle"
         variant="underlined"
-        color="secondary"
+        color="hsla(160, 100%, 37%, 1)"
         :hint="hintText"
         clearable
+        persistent-clear
+        clear-icon="fas fa-times"
         autofocus
         active
         :prefix="exampleQuestionPrefix"
         :suffix="exampleQuestionSuffix"
         :rules="[rules.required]"
-        validate-on="input"
+        validate-on="blur"
+        :append-icon="questionIcon"
       >
       </v-text-field>
       <v-text-field
-        class="centered-input"
+        :class="displayStyle"
         variant="underlined"
-        color="secondary"
+        color="hsla(160, 100%, 37%, 1)"
         :hint="hintText"
         clearable
-        autofocus
+        persistent-clear
+        clear-icon="fas fa-times"
         active
         :prefix="exampleQuestionPrefix"
         :suffix="exampleQuestionSuffix"
         :rules="[rules.required]"
-        validate-on="input"
+        validate-on="blur"
+        :append-icon="questionIcon"
       >
       </v-text-field>
     </v-responsive>
@@ -34,10 +39,27 @@
 </template>
 
 <script setup lang="ts">
+import { useDisplay } from "vuetify";
+import { computed } from "vue";
+
+const { name } = useDisplay();
 const hintText = "Gerundio...";
 const exampleQuestionPrefix = "Eu estou";
 const exampleQuestionSuffix = "meu jantar";
 const correctAnswer = "comendo";
+const questionIcon = "fas fa-utensils";
+
+const displayStyle = computed(() => {
+  switch (name.value) {
+    case "xs":
+      return "center-text-input input-font-xs additional-text";
+    case "sm":
+      return "center-text-input input-font-sm additional-text";
+    case "md":
+      return "center-text-input input-font-md additional-text";
+  }
+  return "center-text-input input-font additional-text";
+});
 
 const rules = {
   required: (value: string) =>
@@ -48,10 +70,42 @@ const rules = {
 <style scoped lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Caveat:wght@500&display=swap");
 
-.hand-written {
+.hand-written-font {
   font-family: "Caveat", cursive;
+  color: hsla(160, 100%, 37%, 1);
 }
-.centered-input :deep(input) {
+
+.center-text-input :deep(input) {
   text-align: center;
 }
+
+.input-font-xs :deep(input) {
+  font-size: 1.2em;
+}
+
+.input-font-sm :deep(input) {
+  font-size: 1.5em;
+}
+
+.input-font-md :deep(input) {
+  font-size: 2em;
+}
+
+.input-font :deep(input) {
+  font-size: 2.5em;
+}
+
+.additional-text :deep(.v-text-field__prefix),
+:deep(.v-text-field__suffix) {
+  font-size: 1.2em;
+}
+
+.additional-text :deep(.v-icon) {
+  padding-top: 15px;
+}
+
+// not currently doing anything:
+/* .additional-text :deep(.v-input__details) {
+  font-size: 1.5em;
+} */
 </style>
